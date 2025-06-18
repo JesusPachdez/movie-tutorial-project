ab23f88d83036939de86e2ce96b13fb5254eae78
Jesús Pacheco Valdez <pacheco1290@gmail.com>
2025-06-17 22:41:42 -0600

    feat: add movie search with real-time filtering

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
 import "./App.css";
-import MovieCard from "./components/MovieCard";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <>
-      <MovieCard
-        movie={{
-          title: "The Dark Knight",
-          release_date: "2008",
-        }}
-      />
-      <MovieCard
-        movie={{
-          title: "spider man no way home",
-          release_date: "2021",
-        }}
-      />
+      <Home />
     </>
   );
 }
