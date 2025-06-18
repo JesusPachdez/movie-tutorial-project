5166b2ce2100ee74cf0a306fb9620509e629e3c9
Jesús Pacheco Valdez <pacheco1290@gmail.com>
2025-06-17 23:04:18 -0600

    feat: implement client-side routing with React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
 import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <>
-      <Home />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
     </>
   );
 }
