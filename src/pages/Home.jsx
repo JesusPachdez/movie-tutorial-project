c5b8f9fa201616a7242d71a2e75ff4c0eebfb0fc
JesusPachdez <pacheco1290@gmail>
2025-06-17 22:41:42 -0600

    feat: add movie search with real-time filtering

---
+++ src/pages/Home.jsx
@@ -0,0 +1,55 @@
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+
+export default function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const movies = [
+    {
+      id: 1,
+      title: "Jhone Wick",
+      release_date: "2014",
+    },
+    {
+      id: 2,
+      title: "spider man no way home",
+      release_date: "2021",
+    },
+    {
+      id: 3,
+      title: "avengers: endgame",
+      release_date: "2019",
+    },
+  ];
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log(searchQuery);
+  };
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <div className="movie-grid">
+        {movies.map(
+          (movie) =>
+            movie.title.toLowerCase().startsWith(searchQuery) && (
+              <MovieCard movie={movie} key={movie.id} />
+            )
+        )}
+      </div>
+    </div>
+  );
+}
